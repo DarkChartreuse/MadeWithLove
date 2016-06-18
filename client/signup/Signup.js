c4fb7adbfbd122e3392e54c6840087645784bc7e
Dan Sajjad <dan.sajjad@gmail.com>
2016-06-17 17:10:49 -0700

    Create skeletion of signup component

---
+++ client/signup/Signup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import  { Link, browserHistory } from 'react-router';
+
+export default class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
