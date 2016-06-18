451a3b25ef4503130c8aee8a5c111c681c68a74f
Dan Sajjad <dan.sajjad@gmail.com>
2016-06-18 11:31:13 -0700

    Add basic styling

--- Client/app/app.js
+++ Client/app/app.js
@@ -5,12 +5,16 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<div>
-			  <h1>trying stuff</h1>
-        <ul role="nav">
-        <li style={{display: 'inline', marginRight:'10px'}}><Link to="/search" activeClassName="active">Search</Link></li>
-        <li style={{display: 'inline', marginRight:'10px'}}><Link to="/signup" activeClassName="active">Sign-up</Link></li>
-        </ul>
-        {this.props.children}
+        <div className="jumbotron">
+          <h1>Made With Love</h1>
+          <p>Home made meals delivered</p>
+          <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+        </div>
+          <ul role="nav" className="nav nav-tabs">
+          <li style={{display: 'inline', marginRight:'10px'}}><Link to="/signup" activeClassName="active">Sign-up</Link></li>
+          <li style={{display: 'inline', marginRight:'10px'}}><Link to="/search" activeClassName="active">Search Chefs</Link></li>
+          </ul>
+          {this.props.children}
 			</div>
 		)
 	}
