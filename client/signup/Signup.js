dd939c8bcfaf895ce6922892eeaea8dd8098f0aa
Dan Sajjad <dan.sajjad@gmail.com>
2016-06-17 17:14:13 -0700

    Add _handleSubmit

--- client/signup/Signup.js
+++ client/signup/Signup.js
@@ -13,6 +13,41 @@ export default class Signup extends React.Component {
     }    
   }
 
+  _handleSubmit() {
+    // validation
+    console.log('i was hit');
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    console.log('checking password');
+    //check if the passwords entered matches
+    if (this.state.password !== this.state.passwordConfirm) {
+      console.log('PASSWORDS DO NOT MATCH');
+      this.setState({error: true, errorMessage: 'passwords do not match'});
+    }
+    console.log('checking email');
+    //check if the email supplied is valid
+    if (!re.test(this.state.email)) {
+      this.setState({error: true, errorMessage: 'invalid email'});
+    }
+
+    console.log('getting ready to send to server:', this.state);
+    // if everything passes, then do the ajax request
+      if (this.state.firstName !== '' && this.state.lastName !== ''){
+        console.log('setting up data:');
+        var data = {
+          firstName: this.state.firstName,
+          lastName: this.state.lastName,
+          password: this.state.password,
+          email: this.state.email
+        };
+        console.log('sending to server');
+        auth.signup(data, (resDB)=>{
+          console.log('++++++++++signed up', resDB);
+          location.reload();
+        });
+      }
+  }
+
   render() {
     return (
       <div id="signup">
