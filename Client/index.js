9337f7c2a9726524baf421f341af64ac8a839a1a
Dan Sajjad <dan.sajjad@gmail.com>
2016-06-17 18:57:07 -0700

    Add Router to index.js

--- Client/index.js
+++ Client/index.js
@@ -1,5 +1,16 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './app/app';
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
+import App from './app/App'
+// import Search from './search/Search'
+import Signup from './signup/Signup'
 
-render(<App />, document.getElementById('app'));
+render((
+  <Router history={hashHistory}>
+    <Route path='/' component={App}>
+      <Route path='/signup' component={Signup} />
+    </Route>
+  </Router>
+), document.getElementById('app'));
+
+// <Route path='/search' component={Search} />
