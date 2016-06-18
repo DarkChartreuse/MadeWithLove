63186ffce372f1535ed6a49c1d4b9a31d0c6b9a6
Dan Sajjad <dan.sajjad@gmail.com>
2016-06-17 18:57:07 -0700

    Add Router to index.js

--- Client/app/app.js
+++ Client/app/app.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router'
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div>
 			  <h1>trying stuff</h1>
+        <ul role="nav">
+        <li style={{display: 'inline', marginRight:'10px'}}><Link to="/search" activeClassName="active">Search</Link></li>
+        <li style={{display: 'inline', marginRight:'10px'}}><Link to="/signup" activeClassName="active">Sign-up</Link></li>
+        </ul>
+        {this.props.children}
 			</div>
 		)
 	}
