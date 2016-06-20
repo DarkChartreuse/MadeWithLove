aca3d2a57249b5c7643e9ae78288648264a75aaf
Dan Sajjad <dan.sajjad@gmail.com>
2016-06-20 16:24:19 -0700

    Add dummy cuisine data

--- client/search/Search.js
+++ client/search/Search.js
@@ -7,8 +7,16 @@ export default class Search extends React.Component {
   }
 
   render() {
-    return (
-      <div>Search feature will be coming soon!</div>
-    );
+    var Cuisines = [
+      {category: 'Japanese', price: '$49.99', stocked: true, name: 'Sushi'},
+      {category: 'Japanese', price: '$9.99', stocked: true, name: 'Dango'},
+      {category: 'Japanese', price: '$29.99', stocked: false, name: 'Ramen'},
+      {category: 'Indian', price: '$99.99', stocked: true, name: 'Palak Paneer'},
+      {category: 'Indian', price: '$399.99', stocked: false, name: 'Chicken Tikka'},
+      {category: 'Indian', price: '$199.99', stocked: true, name: 'Balti Chicken'}
+    ];    
+    return <FilterableCuisineTable cuisines={Cuisines} />;
   }
 }
+
+
