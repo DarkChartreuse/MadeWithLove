b4c8d2fe5b2d5251b5ff318937e2883251ac948a
dfuruya <dfuruya@gmail.com>
2016-06-25 15:16:52 -0700

    Add Order model and controller

--- server/db/models/ordersModel.js
+++ server/db/models/ordersModel.js
@@ -11,42 +11,46 @@ const Order = sequelize.define('orders',
       type: Sequelize.INTEGER,
       field: 'id_chef',
     },
-    food: {
+    mealId: {
       type: Sequelize.STRING,
-      field: 'food',
+      field: 'id_meal',
     },
-    cuisine: {
-      type: Sequelize.STRING,
-      field: 'cuisine',
-    },
-    description: {
-      type: Sequelize.STRING,
-      field: 'description',
-    },
-    quantity: {
-      type: Sequelize.INTEGER,
-      field: 'quantity',
-    },
-    price: {
-      type: Sequelize.FLOAT,
-      field: 'price',
-    },
-    address: {
-      type: Sequelize.STRING,
-      field: 'address',
-    },
-    orderDate: {
-      type: Sequelize.DATE,
-      field: 'order_date',
-    },
-    rating: {
-      type: Sequelize.FLOAT,
-      field: 'rating',
-    },
-    review: {
-      type: Sequelize.TEXT,
-      field: 'review',
-    },
+    // food: {
+    //   type: Sequelize.STRING,
+    //   field: 'food',
+    // },
+    // cuisine: {
+    //   type: Sequelize.STRING,
+    //   field: 'cuisine',
+    // },
+    // description: {
+    //   type: Sequelize.STRING,
+    //   field: 'description',
+    // },
+    // quantity: {
+    //   type: Sequelize.INTEGER,
+    //   field: 'quantity',
+    // },
+    // price: {
+    //   type: Sequelize.FLOAT,
+    //   field: 'price',
+    // },
+    // address: {
+    //   type: Sequelize.STRING,
+    //   field: 'address',
+    // },
+    // orderDate: {
+    //   type: Sequelize.DATE,
+    //   field: 'order_date',
+    // },
+    // rating: {
+    //   type: Sequelize.FLOAT,
+    //   field: 'rating',
+    // },
+    // review: {
+    //   type: Sequelize.TEXT,
+    //   field: 'review',
+    // },
   },
   {
     timestamps: true,
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 const User = require('./db/controllers/usersController.js');
 const Meal = require('./db/controllers/mealsController.js');
+const Order = require('./db/controllers/ordersController.js');
 const Controller = require('./db/controllers/controller.js');
 const passport = require('passport');
 
@@ -34,6 +35,8 @@ module.exports = (app) => {
   app.get('/api/meals', Meal.getAllMeals);
   app.get('/api/meals/:id', Meal.getMeal);
   app.delete('/api/meals/:id', Meal.deleteMeal);
+
+  app.post('/api/createOrder', Order.createOrder);
 
   app.get('/api/meal/:id', Controller.getMealView);
 
