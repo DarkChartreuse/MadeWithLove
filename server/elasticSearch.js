5c7bf1b64fd153ba00674d43339aa58e7402e84b
dfuruya <dfuruya@gmail.com>
2016-07-01 10:38:54 -0700

    Adjust docker settings

--- server/elasticSearch.js
+++ server/elasticSearch.js
@@ -3,7 +3,6 @@ var generateEmail = require('./emailGenerator.js');
 
 var elasticAddress = process.env.MWLES_PORT_9200_TCP_ADDR || 'localhost';
 
-<<<<<<< 0f9676418bac656bea92a7fffcba4ba73d6df834
 var ESDB_HOST;
 
 ESDB_HOST = process.env.NODE_ENV === 'production' ? 'elasticdb' : 'localhost';
@@ -12,12 +11,6 @@ console.log('>>>>>>>>>>>>> elasticsearch: ', ESDB_HOST);
 
 var elasticClient = new elasticsearch.Client({
   host: ESDB_HOST + ':9200',
-=======
-console.log('>>>>>>>>>>>>> elasticsearch: ', elasticAddress);
-
-var elasticClient = new elasticsearch.Client({
-  host: elasticAddress + ':9200',
->>>>>>> Adjust docker settings
   log: 'info'
 });
 
